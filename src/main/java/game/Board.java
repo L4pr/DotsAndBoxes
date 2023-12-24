06169c0cdcba43e5764a8f84aeaff33bda8e1806
RenzoL4pr <556140@student.saxion.nl>
2023-12-24 14:35:54 +0100

    initial commit

---
+++ src/main/java/game/Board.java
@@ -0,0 +1,15 @@
+package game;
+
+public class Board {
+
+    // first 30 bits horizontal, next 30 bits horizontal
+    private long lines = 0;
+
+
+    public void setLine(int coords) {
+
+    }
+
+
+
+}
